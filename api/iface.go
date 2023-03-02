0a3e0a7343164d2f4da38d59a9ec93df8a587cab
aoi shirase <ieeex024x@gmail.com>
2023-03-03 07:46:57 +0900

    rename and add update interface

--- api/iface.go
+++ api/iface.go
@@ -2,4 +2,5 @@ package api
 
 type OpenAIAPIIface interface {
 	ListModelsV1(*ListModelsV1Input) (*ListModelsV1Output, error)
+	ChatCompletionsV1(input *ChatCompletionsV1Input) (*ChatCompletionsV1Output, error)
 }
