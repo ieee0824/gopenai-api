a44fbf855c1415f00d0e184349629a654a18b7b4
aoi shirase <ieee0824x@gmail.com>
2023-03-02 20:12:00 +0900

    v1 list models

--- api/api.go
+++ api/api.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/ieee0824/gopenai-api/config"
 )
@@ -17,4 +20,23 @@ func New(cfg *config.Configuration) *OpenAIAPI {
 		httpClient:    http.DefaultClient,
 		configuration: cfg,
 	}
+}
+
+func (api *OpenAIAPI) endpoint() (*url.URL, error) {
+	if api.configuration.Endpoint == nil {
+		return url.Parse("https://api.openai.com")
+	}
+	return url.Parse(*api.configuration.Endpoint)
+}
+
+func (api *OpenAIAPI) setToken(req *http.Request) error {
+	if api.configuration.ApiKey == nil {
+		return errors.New("no token")
+	}
+	if api.configuration.Organization == nil {
+		return errors.New("no organization")
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *api.configuration.ApiKey))
+	req.Header.Add("OpenAI-Organization", *api.configuration.Organization)
+	return nil
 }
--- api/v1_models.go
+++ api/v1_models.go
@@ -1,1 +1,91 @@
 package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ListModelsV1Permission struct {
+	ID                 string `json:"id,omitempty"`
+	Object             string `json:"object,omitempty"`
+	Created            int    `json:"created,omitempty"`
+	AllowCreateEngine  bool   `json:"allow_create_engine,omitempty"`
+	AllowSampling      bool   `json:"allow_sampling,omitempty"`
+	AllowLogprobs      bool   `json:"allow_logprobs,omitempty"`
+	AllowSearchIndices bool   `json:"allow_search_indices,omitempty"`
+	AllowView          bool   `json:"allow_view,omitempty"`
+	AllowFineTuning    bool   `json:"allow_fine_tuning,omitempty"`
+	Organization       string `json:"organization,omitempty"`
+	Group              any    `json:"group,omitempty"`
+	IsBlocking         bool   `json:"is_blocking,omitempty"`
+}
+
+type ListModelsV1Data struct {
+	ID         string                   `json:"id,omitempty"`
+	Object     string                   `json:"object,omitempty"`
+	Created    int                      `json:"created,omitempty"`
+	OwnedBy    string                   `json:"owned_by,omitempty"`
+	Permission []ListModelsV1Permission `json:"permission,omitempty"`
+	Root       string                   `json:"root,omitempty"`
+	Parent     any                      `json:"parent,omitempty"`
+}
+
+type ListModelsV1Output struct {
+	Error  *Error             `json:"error,omitempty"`
+	Object string             `json:"object,omitempty"`
+	Data   []ListModelsV1Data `json:"data,omitempty"`
+}
+
+func (impl *ListModelsV1Output) String() string {
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(impl)
+	return buf.String()
+}
+
+func (api *OpenAIAPI) ListModelsV1() (*ListModelsV1Output, error) {
+	endpoint, err := api.endpoint()
+	if err != nil {
+		return nil, err
+	}
+	endpoint.Path = "/v1/models"
+	req, err := http.NewRequest(
+		http.MethodGet,
+		endpoint.String(),
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	api.setToken(req)
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		ret := &ListModelsV1Output{}
+		if err := json.NewDecoder(resp.Body).Decode(ret); err != nil {
+			return nil, err
+		}
+		return ret, nil
+	case http.StatusUnauthorized:
+		ret := &ListModelsV1Output{}
+		if err := json.NewDecoder(resp.Body).Decode(ret); err != nil {
+			return nil, err
+		}
+		return ret, ErrUnauthorized
+	default:
+		buf := new(bytes.Buffer)
+		io.Copy(buf, resp.Body)
+		ret := &ListModelsV1Output{
+			Error: &Error{
+				Message: buf.String(),
+			},
+		}
+		return ret, ErrUnknown
+	}
+}
